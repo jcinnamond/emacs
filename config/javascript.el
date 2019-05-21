9156d8d9e664fda4674b55ec9a1f72e6e321273b
John Cinnamond <j.cinnamond@iand.com>
2019-05-21 14:41:45 +0100

    Add key bindings for running tests from javascript

--- config/javascript.el
+++ config/javascript.el
@@ -1,3 +1,9 @@
 (use-package js
   :config
+  (define-prefix-command 'js-comma-map)
+  (evil-define-key '(normal visual motion emacs) js-mode-map (kbd ",") 'js-comma-map)
+  (define-key 'js-comma-map (kbd "t t") 'dockspec-run-current-file)
+  (define-key 'js-comma-map (kbd "t s") 'dockspec-run-current-line)
+  (define-key 'js-comma-map (kbd "t a") 'dockspec-run-all)
+  (define-key 'js-comma-map (kbd "t r") 'dockspec-rerun)
   (setq-default js-indent-level 2))
