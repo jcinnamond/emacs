86f560732c679c2f3abb69dc766f954209b4dbc9
John Cinnamond <j.cinnamond@iand.com>
2019-05-24 17:56:12 +0100

    Collapse nested ifs when checking for rspec files

--- config/ruby.el
+++ config/ruby.el
@@ -14,9 +14,8 @@
   (add-hook 'ruby-mode-hook 'flycheck-mode)
   (add-hook 'ruby-mode-hook 'seeing-is-believing)
   (add-hook 'ruby-mode-hook (lambda ()
-			      (if (buffer-file-name)
-				  (if (string-match "_spec\\.rb\\'" buffer-file-name)
-				      (yas-activate-extra-mode 'ruby-testing))))))
+			      (if (and (buffer-file-name) (string-match "_spec\\.rb\\'" buffer-file-name))
+				      (yas-activate-extra-mode 'ruby-testing)))))
 
 (use-package ruby-end
   :ensure t)
