790ab53b1e7f464edc75b91d2ce32c983d1f7b98
John Cinnamond <j.cinnamond@iand.com>
2019-06-11 09:56:14 +0100

    Use goimports for formatting & cleaning up imports.

--- config/go.el
+++ config/go.el
@@ -6,10 +6,12 @@
   (evil-define-key '(normal visual motion emacs) go-mode-map (kbd ",") 'go-comma-map)
   (define-key 'go-comma-map (kbd "d") 'godoc-at-point)
   (define-key 'go-comma-map (kbd "D") 'godoc)
+  (define-key 'go-comma-map (kbd "f") 'gofmt)
   (add-hook 'go-mode-hook (lambda () (interactive)
 			    (setq tab-width 4)
 			    (setq indent-tabs-mode 1)
 			    (set (make-local-variable 'company-backends) '(company-go))))
+  (setq gofmt-command "goimports")
   (add-hook 'before-save-hook 'gofmt-before-save))
 
 (use-package company-go
@@ -19,5 +21,6 @@
   :hook go-mode
   :bind (:map go-comma-map
 	      ("c" . gorun-build)
+	      ("t r" . gorun-rerun)
 	      ("t s" . gorun-test-single)
 	      ("t t" . gorun-test-all)))
