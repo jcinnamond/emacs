90ea611e09ac6e8c1c39977bf64cd9422113acc0
John Cinnamond <jcinnamond@gmail.com>
2019-06-23 18:08:39 +0100

    Reduce lsp-ui popups and overlays

--- config/go.el
+++ config/go.el
@@ -4,10 +4,9 @@
   (setq exec-path (append exec-path '("~/go/bin")))
   (define-prefix-command 'go-comma-map)
   (evil-define-key '(normal visual motion emacs) go-mode-map (kbd ",") 'go-comma-map)
-  (define-key 'go-comma-map (kbd "d") 'godoc-at-point)
-  (define-key 'go-comma-map (kbd "D") 'godoc)
-  (define-key 'go-comma-map (kbd "f") 'gofmt)
-  (add-hook 'ruby-mode-hook 'flyspell-prog-mode)
+  (define-key 'go-comma-map (kbd "d") 'lsp-ui-doc-show)
+  (define-key 'go-comma-map (kbd "q") 'lsp-ui-doc-hide)
+  (add-hook 'go-mode-hook 'flyspell-prog-mode)
   (add-hook 'go-mode-hook (lambda () (interactive)
 			    (setq tab-width 4)
 			    (setq indent-tabs-mode 1)))
@@ -32,7 +31,10 @@
   :hook (go-mode . lsp))
 
 (use-package lsp-ui
-  :ensure t)
+  :ensure t
+  :config
+  (setq-default lsp-ui-sideline-show-hover nil)
+  (setq-default lsp-ui-doc-enable nil))
 
 (use-package company-lsp
   :ensure t)
