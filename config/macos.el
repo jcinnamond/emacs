c28f30e9b8fa73763674de83d3ddabf458b9e1ea
John <johncinnamond@monzo.com>
2019-07-21 15:32:53 +0100

    Add homedir bin to path

--- config/macos.el
+++ config/macos.el
@@ -1,3 +1,4 @@
 (set-face-attribute 'default nil :height 130)
-(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
-(setq exec-path (append exec-path '("/usr/local/bin")))
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin:~/bin"))
+(setenv "GOPATH" "/Users/johncinnamond")
+(setq exec-path (append exec-path '("/usr/local/bin" "~/bin")))
