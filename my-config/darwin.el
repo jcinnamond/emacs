1af40529b35a07b0ca01da83be711a4490459078
John <johncinnamond@monzo.com>
2019-07-22 10:20:21 +0100

    Fix paths and # keybinding on mac
    Some paths have moved to `local` because they contain the hardcoded
    full path to my home directory.

--- my-config/darwin.el
+++ my-config/darwin.el
@@ -1,8 +1,11 @@
 (set-face-attribute 'default nil :height 130)
 
-(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin:~/bin"))
-(setq exec-path (append exec-path '("/usr/local/bin" "~/bin")))
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
+(setq exec-path (append exec-path '("/usr/local/bin")))
 
 (setq-default darwin-config-loaded "true")
 
 (setq ispell-program-name "/usr/local/bin/aspell")
+
+;; The key combination for # on a mac with UK layout it option-3 because :shrug:
+(global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#")))
