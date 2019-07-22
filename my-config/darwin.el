9b9042997ed06f4e299fc01cc1e8473cfe38aeb0
John <johncinnamond@monzo.com>
2019-07-22 15:11:05 +0100

    Move bin directory under homedir back into PATH on macs

--- my-config/darwin.el
+++ my-config/darwin.el
@@ -1,7 +1,7 @@
 (set-face-attribute 'default nil :height 130)
 
-(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
-(setq exec-path (append exec-path '("/usr/local/bin")))
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin" (concat ":" (getenv "HOME") "/bin")))
+(setq exec-path (append exec-path '("/usr/local/bin" (concat ":" (getenv "HOME") "/bin"))))
 
 (setq-default darwin-config-loaded "true")
 
