ab790520f7a0e08e64faa759c4bcf5ac6224733a
John Cinnamond <johncinnamond@monzo.com>
2020-04-06 11:38:56 +0100

    Update protobuf mode
    It stopped working, possibly because I'm using a pre-release version of emacs 28

--- elpa/protobuf-mode-20170526.1650/protobuf-mode-pkg.el
+++ elpa/protobuf-mode-20170526.1650/protobuf-mode-pkg.el
@@ -1,2 +1,2 @@
 ;;; -*- no-byte-compile: t -*-
-(define-package "protobuf-mode" "20170526.1650" "major mode for editing protocol buffers." 'nil :commit "744e799bdaad748a6069a1f3fe8a25c708317c25" :keywords '("google" "protobuf" "languages") :authors '(("Alexandre Vassalotti" . "alexandre@peadrop.com")) :maintainer '("Alexandre Vassalotti" . "alexandre@peadrop.com"))
+(define-package "protobuf-mode" "20170526.1650" "major mode for editing protocol buffers." 'nil :commit "dec4939439d9ca2adf2bb14edccf876c2587faf2" :keywords '("google" "protobuf" "languages") :authors '(("Alexandre Vassalotti" . "alexandre@peadrop.com")) :maintainer '("Alexandre Vassalotti" . "alexandre@peadrop.com"))
