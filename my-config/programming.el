16a1b0edb8b58e25d83dc6a32e5179b664515a2d
John Cinnamond <john@cinnamond.me.uk>
2020-06-19 16:49:39 +0100

    Reuse Go Test window in other frames

--- my-config/programming.el
+++ my-config/programming.el
@@ -74,6 +74,9 @@
               ("l R" . lsp-workspace-restart)
               ("l t" . lsp-find-type-definition)))
 
+;; When running go tests, use an existing buffer in another frame for output if available
+(add-to-list 'display-buffer-alist '(("\* Go Test \*" nil (reusable-frames . t))))
+
 ;;;----------------------------------------------------------------------
 ;;; Editing protobuf files
 
