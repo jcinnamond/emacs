d0cca7719ed8b1ae303c94af68912af36a6cc284
John Cinnamond <jcinnamond@gmail.com>
2020-07-18 16:23:04 +0100

    Autoscroll compilation output by default

--- my-config/programming.el
+++ my-config/programming.el
@@ -87,6 +87,7 @@
 ;; When running go tests, use an existing buffer in another frame for output if available
 (add-to-list 'display-buffer-alist '("\* Go Test \*" nil (reusable-frames . t)))
 (add-to-list 'display-buffer-alist '("\*compilation\*" nil (reusable-frames . t)))
+(setq-default compilation-scroll-output t)
 
 ;;;----------------------------------------------------------------------
 ;;; Editing protobuf files
